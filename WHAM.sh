e1a088fb8b880b6ee8cba8cc5f30ef24f12d35d2
abogutz <37719032+abogutz@users.noreply.github.com>
2024-03-21 14:12:32 -0700

    dip

--- WHAM.sh
+++ WHAM.sh
@@ -126,6 +126,9 @@ function parseOptions () {
 				mkdir $SCRATCH_DIR
 				TEMP1=$SCRATCH_DIR"/temp"
 				TEMP2=$SCRATCH_DIR"/temp2"
+				TEMP3=$SCRATCH_DIR"/temp3"
+				TEMP4=$SCRATCH_DIR"/temp4"
+				TEMP5=$SCRATCH_DIR"/temp5"
 				PE_BAM=$SCRATCH_DIR"/temp.bam"
 				;;
 			t) #threads
@@ -218,7 +221,7 @@ function dipTest () {
 	echo "Making genomic windows..."
 	if [[ $REF_BED == "" ]] ; then # If not using input bed, then bin genome
 		REF_BED=$SCRATCH_DIR"/ref"-$DIPTEST_BINSIZE"bp.bed"
-		bedtools makewindows -w $DIPTEST_BINSIZE -b $GENOME_BED | sort -k1,1 -k2,2n - > $REF_BED
+		bedtools makewindows -w $DIPTEST_BINSIZE -b $GENOME_BED | awk 'OFS="\t"{if($1 ~ /chr[0-9XY]*$/) {print $0}}' | sort -k1,1 -k2,2n - > $REF_BED
 	else # Using input bed file
 		REF_BED_NAME=${REF_BED%%.*}
 		DIPTEST_OUTPUT=$GENOME_DIR$NAME"_"$REF_BED_NAME"-"$MIN_CPG"CpG-diptest.bw"
